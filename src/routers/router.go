a698e1645d5e82a441743c0ac44f6f566ea4a827
anaana35 <xl04754442@outlook.com>
2019-11-09 17:22:55 +0800

    import

--- src/routers/router.go
+++ src/routers/router.go
@@ -8,4 +8,5 @@ import (
 func init() {
     beego.Router("/", &controllers.MainController{})
     beego.Router("/homepage", &controllers.HomePageController{})
+	beego.Router("/import",&controllers.ImportController{})
 }
