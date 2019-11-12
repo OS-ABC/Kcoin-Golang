2477d2f5547e927c7ebfb40153de5151c39f97ef
Guibeen <guibin@pku.edu.cn>
2019-11-12 08:49:58 +0800

    修改controller文件，使页面内容能显示完全

--- src/controllers/personalProjects.go
+++ src/controllers/personalProjects.go
@@ -15,52 +15,51 @@ func (c *PersonalProjectsController) Get() {
     jsonBuf :=
     `{
     "errorCode": "0",
-    "data": [
-        {
-            "projectName": "天气预报1",
-            "projectCoverUrl": "../static/img/projectbg.png",
-            "projectUrl": "",
-            "memberList": [
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx2.png"
-                },
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx1.png"
-                }
-            ]
-        },
-        {
-            "projectName": "天气预报2",
-            "projectCoverUrl": "../static/img/projectbg.png",
-            "projectUrl": "",
-            "memberList": [
-                {
-                    "userName": "Joy",
-                    "headshotUrl": "../static/img/tx1.png"
-                },
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx2.png"
-                }
-            ]
-        }
-    ]
+    "data": {
+        "userName": "Cyan",
+        "headshotUrl": "../static/img/tx1.png",
+        "projectList":
+        [
+            {
+                "projectName": "天气预报1",
+                "projectCoverUrl": "../static/img/projectbg.png",
+                "projectUrl": "",
+                "memberList": [
+                    {
+                        "userName": "Tony",
+                        "headshotUrl": "../static/img/tx2.png"
+                    },
+                    {
+                        "userName": "Tony",
+                        "headshotUrl": "../static/img/tx1.png"
+                    }
+                ]
+            },
+            {
+                "projectName": "天气预报2",
+                "projectCoverUrl": "../static/img/projectbg.png",
+                "projectUrl": "",
+                "memberList": [
+                    {
+                        "userName": "Joy",
+                        "headshotUrl": "../static/img/tx1.png"
+                    },
+                    {
+                        "userName": "Tony",
+                        "headshotUrl": "../static/img/tx2.png"
+                    }
+                ]
+            }
+        ]
+    }
     }`
 
     var user UserInfo
-    var proj Result
-    errorCode1 := json.Unmarshal([]byte(jsonBuf), &user)
-    errorCode2 := json.Unmarshal([]byte(jsonBuf), &proj)
-	if errorCode1 != nil {
-		fmt.Println("Oops, there is an error:( please keep debugging.", errorCode1.Error())
-    }
-	if errorCode2 != nil {
-		fmt.Println("Oops, there is an error:( please keep debugging.", errorCode2.Error())
+    errorCode := json.Unmarshal([]byte(jsonBuf), &user)
+	if errorCode != nil {
+		fmt.Println("Oops, there is an error:( please keep debugging.", errorCode.Error())
     }
 
 	c.Data["user"] = user
-	c.Data["Projects"] = proj
 	c.TplName = "personalProjects.html"
 }
