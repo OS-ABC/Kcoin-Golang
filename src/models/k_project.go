436336854a8b4870990afcffca711ddcffae17f1
yyl9510 <584540273@qq.com>
2019-11-14 18:34:44 +0800

    modify function GetAllProjectsInfo() to change the return type

--- src/models/k_project.go
+++ src/models/k_project.go
@@ -14,24 +14,36 @@ type KProject struct {
 	ProjectCoverUrl string `json:"pcurl"`
 }
 
+type projectInfo struct {
+	ErrorCode string     `json:"errorCode"`
+	Projects  []KProject `json:"data"`
+}
+
 func init() {
 	// 需要在init中注册定义的model
 	orm.RegisterModel(new(KProject))
 }
 
 //查询并以json形式返回所有的项目信息
-func GetAllProjectsInfo() string {
+func GetAllProjectsInfo() (string, error) {
 	o := orm.NewOrm()
 	o.Using("default")
 
-	var projects []KProject
-	_, err := o.Raw("SELECT * FROM \"K_Project\"").QueryRows(&projects)
+	var projectsInfo projectInfo
+	projectsInfo.ErrorCode = "default Error"
+
+	//var projects []KProject
+	querySql := `SELECT * FROM "K_Project"`
+	_, err := o.Raw(querySql).QueryRows(&projectsInfo.Projects)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
+		return fmt.Sprint(projectsInfo), err
 	}
-	res, err := json.Marshal(&projects)
+	projectsInfo.ErrorCode = "0"
+	res, err := json.Marshal(&projectsInfo)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
+		return fmt.Sprint(projectsInfo), err
 	}
-	return string(res)
+	return string(res), nil
 }
