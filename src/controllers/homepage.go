f5d50633de1a1a129d3656ed5454bb78dc1841db
Darkone0 <jialc@pku.edu.cn>
2019-11-15 12:51:51 +0800

    根据登陆状态动态显示首页nav

--- src/controllers/homepage.go
+++ src/controllers/homepage.go
@@ -112,19 +112,35 @@ func (c *HomePageController) Get() {
     //]
     //}`
 
+    //从cookie中取出状态
+    isLogin :=c.Ctx.GetCookie("status")
+
+
     //把Json字符串中的数据解析到结构体中
     var proj ProjectListInfo
-    jsonBuf , _ := models.GetAllProjectsInfo()
-    err := json.Unmarshal([]byte(jsonBuf),&proj)
+    projectBuf , _ := models.GetAllProjectsInfo()
+    err := json.Unmarshal([]byte(projectBuf),&proj)
     if err != nil {
         fmt.Println("err=", err)
-        return
+        //return
     }
+
 
     //把结构体传到模板当中
     c.Data["Projects"] = proj
-    //设置当前登录状态
-    c.Data["isLogin"] = true
+
+    //判断当前登录状态
+    if isLogin == "1"{
+        c.Data["isLogin"] = true
+        var user UserInfo//user中存放着json解析后获得的数据。
+        user.Data.UserName = c.Ctx.GetCookie("userName")
+        user.Data.HeadShotUrl = c.Ctx.GetCookie("headShotUrl")
+        c.Data["user"] = user
+    } else {
+        c.Data["isLogin"] = false
+    }
+
+
     //设置Get方法对应展示的模板
     c.TplName = "homePage.tpl"
 }
