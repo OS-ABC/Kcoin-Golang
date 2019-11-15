2359096839598036b3feaa302bac3deb137622c7
Darkone0 <jialc@pku.edu.cn>
2019-11-15 12:12:21 +0800

    修改models/k_project.go 规范前端接口

--- src/models/k_project.go
+++ src/models/k_project.go
@@ -14,7 +14,22 @@ type KProject struct {
 	ProjectCoverUrl string `json:"pcurl"`
 }
 
+type Project struct {
+	ProjectName string          `json:"projectName"`
+	ProjectCoverUrl string      `json:"projectCoverUrl"`
+	ProjectUrl string           `json:"projectUrl"`
+	MemberList []UserData       `json:"memberList"`
+}
+type UserData struct {
+	UserName    string    `json:"userName"`
+	HeadShotUrl string    `json:"headshotUrl"`
+	ProjectList []Project `json:"projectList"`
+}
 type projectInfo struct {
+	ErrorCode string     `json:"errorCode"`
+	Projects  []Project `json:"data"`
+}
+type kprojectInfo struct {
 	ErrorCode string     `json:"errorCode"`
 	Projects  []KProject `json:"data"`
 }
@@ -25,11 +40,18 @@ func GetAllProjectsInfo() (string, error) {
 	o.Using("default")
 
 	var projectsInfo projectInfo
+	//var projects []Project
 	projectsInfo.ErrorCode = "default Error"
 
 	//var projects []KProject
-	querySql := `SELECT * FROM "K_Project"`
+	querySql := `SELECT project_name , project_url , project_cover_url FROM "K_Project"`
 	_, err := o.Raw(querySql).QueryRows(&projectsInfo.Projects)
+	for i := range projectsInfo.Projects {
+		var u UserData
+		u.HeadShotUrl = "../static/img/tx1.png"
+		u.UserName = "abc"
+		projectsInfo.Projects[i].MemberList = append(projectsInfo.Projects[i].MemberList, u)
+	}
 	if err != nil {
 		fmt.Println(err.Error())
 		return fmt.Sprint(projectsInfo), err
