3f7f69442f999b3a3505c5ad9d34f75223ae0715
Darkone0 <jialc@pku.edu.cn>
2019-11-15 12:07:10 +0800

    调用后端函数动态刷新homepage和personalPage

--- src/controllers/personalPage.go
+++ src/controllers/personalPage.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"Kcoin-Golang/src/models"
+	_ "Kcoin-Golang/src/models"
 	"encoding/json"
 	"fmt"
 
@@ -13,16 +15,16 @@ type PersonalPageController struct {
 
 func (c *PersonalPageController) Get() {
 	//jsonBuf是一个用于调试的静态json，之后会调用webserver的接口，动态获取。
-	jsonBuf :=
-		`{
-		"errorCode": "0",
-		"data":{
-			"userName": "DoubleJ",
-			"headShotUrl": "../static/img/tx2.png"
-		}
-	}`
-
+	//jsonBuf :=
+	//	`{
+	//	"errorCode": "0",
+	//	"data":{
+	//		"userName": "DoubleJ",
+	//		"headShotUrl": "../static/img/tx2.png"
+	//	}
+	//}`
 	var user UserInfo//user中存放着json解析后获得的数据。
+	jsonBuf , _ := models.GetUserInfo("chen_cheng")
 	errorCode := json.Unmarshal([]byte(jsonBuf), &user)//将jsonBuf的数据解析，然后赋值给user，如果出错会返回对应的errorCode
 	if errorCode != nil {//出错了，panic
 		fmt.Println("there is an error ,sorry ,please continue debug,haha", errorCode.Error())
