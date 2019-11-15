9af3de8ab30a6b0c973eb891b61da93fe869cc43
Darkone0 <jialc@pku.edu.cn>
2019-11-15 12:07:10 +0800

    调用后端函数动态刷新homepage和personalPage

--- src/controllers/homepage.go
+++ src/controllers/homepage.go
@@ -1,9 +1,11 @@
 package controllers
 
-import(
-    "github.com/astaxie/beego"
+import (
+    "Kcoin-Golang/src/models"
+    _ "Kcoin-Golang/src/models"
     "encoding/json"
     "fmt"
+    "github.com/astaxie/beego"
 )
 
 type HomePageController struct {
@@ -13,105 +15,106 @@ type HomePageController struct {
 
 func (c *HomePageController) Get() {
     //测试数据
-    jsonBuf :=
-    `{
-    "errorCode": "0",
-    "data": [
-        {
-            "projectName": "天气预报1",
-            "projectCoverUrl": "../static/img/projectbg.png",
-            "projectUrl": "",
-            "memberList": [
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx2.png"
-                },
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx1.png"
-                }
-            ]
-        },
-        {
-            "projectName": "天气预报1",
-            "projectCoverUrl": "../static/img/projectbg.png",
-            "projectUrl": "",
-            "memberList": [
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx2.png"
-                },
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx1.png"
-                }
-            ]
-        },
-        {
-            "projectName": "天气预报1",
-            "projectCoverUrl": "../static/img/projectbg.png",
-            "projectUrl": "",
-            "memberList": [
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx2.png"
-                },
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx1.png"
-                }
-            ]
-        },
-        {
-            "projectName": "天气预报1",
-            "projectCoverUrl": "../static/img/projectbg.png",
-            "projectUrl": "",
-            "memberList": [
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx2.png"
-                },
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx1.png"
-                }
-            ]
-        },
-        {
-            "projectName": "天气预报1",
-            "projectCoverUrl": "../static/img/projectbg.png",
-            "projectUrl": "",
-            "memberList": [
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx2.png"
-                },
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx1.png"
-                }
-            ]
-        },
-        {
-            "projectName": "天气预报2",
-            "projectCoverUrl": "../static/img/projectbg.png",
-            "projectUrl": "",
-            "memberList": [
-                {
-                    "userName": "Joy",
-                    "headshotUrl": "../static/img/tx1.png"
-                },
-                {
-                    "userName": "Tony",
-                    "headshotUrl": "../static/img/tx2.png"
-                }
-            ]
-        }
-    ]
-    }`
+    //jsonBuf :=
+    //`{
+    //"errorCode": "0",
+    //"data": [
+    //    {
+    //        "projectName": "天气预报1",
+    //        "projectCoverUrl": "../static/img/projectbg.png",
+    //        "projectUrl": "",
+    //        "memberList": [
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx2.png"
+    //            },
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx1.png"
+    //            }
+    //        ]
+    //    },
+    //    {
+    //        "projectName": "天气预报1",
+    //        "projectCoverUrl": "../static/img/projectbg.png",
+    //        "projectUrl": "",
+    //        "memberList": [
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx2.png"
+    //            },
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx1.png"
+    //            }
+    //        ]
+    //    },
+    //    {
+    //        "projectName": "天气预报1",
+    //        "projectCoverUrl": "../static/img/projectbg.png",
+    //        "projectUrl": "",
+    //        "memberList": [
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx2.png"
+    //            },
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx1.png"
+    //            }
+    //        ]
+    //    },
+    //    {
+    //        "projectName": "天气预报1",
+    //        "projectCoverUrl": "../static/img/projectbg.png",
+    //        "projectUrl": "",
+    //        "memberList": [
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx2.png"
+    //            },
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx1.png"
+    //            }
+    //        ]
+    //    },
+    //    {
+    //        "projectName": "天气预报1",
+    //        "projectCoverUrl": "../static/img/projectbg.png",
+    //        "projectUrl": "",
+    //        "memberList": [
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx2.png"
+    //            },
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx1.png"
+    //            }
+    //        ]
+    //    },
+    //    {
+    //        "projectName": "天气预报2",
+    //        "projectCoverUrl": "../static/img/projectbg.png",
+    //        "projectUrl": "",
+    //        "memberList": [
+    //            {
+    //                "userName": "Joy",
+    //                "headshotUrl": "../static/img/tx1.png"
+    //            },
+    //            {
+    //                "userName": "Tony",
+    //                "headshotUrl": "../static/img/tx2.png"
+    //            }
+    //        ]
+    //    }
+    //]
+    //}`
 
     //把Json字符串中的数据解析到结构体中
     var proj ProjectListInfo
+    jsonBuf , _ := models.GetAllProjectsInfo()
     err := json.Unmarshal([]byte(jsonBuf),&proj)
     if err != nil {
         fmt.Println("err=", err)
