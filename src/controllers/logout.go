5a3f4cff6ed4f1573fbebc10aecda4615731a1dc
gth <550653445@qq.com>
2019-11-16 17:38:42 +0800

    fixes #62 personalPage和personalProject验证问题

--- src/controllers/logout.go
+++ src/controllers/logout.go
@@ -12,11 +12,9 @@ type LogOutController struct {
 	beego.Controller
 }
 func (c *LogOutController) Get(){
-	//存储用户名到cooike中，获取语法：c.Ctx.GetCookie("userName")
-	c.Ctx.SetCookie("userName"," ",100)
-	//存储用户头像url到cooike中，获取语法：c.Ctx.GetCookie("userName")
-	c.Ctx.SetCookie("headShotUrl"," ",100)
-	//存储用户登录状态到cooike中，其中1表示已登录，获取语法：c.Ctx.GetCookie("userName")
+
+	c.Ctx.SetCookie("userName","",100)
+	c.Ctx.SetCookie("headShotUrl","",100)
 	c.Ctx.SetCookie("status", string('0'),100)
 	fmt.Println(("congrat,his is log out "))
 	c.Redirect("/homepage",302)
