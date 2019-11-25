126a8f380f9b156448bff35948ba798160601c3f
jiangyuzhao <jiangyuzhao@pku.edu.cn>
2019-11-25 21:55:05 +0800

    完成github授权方面的代码重构, 并加入给特定项目添加webhooks功能.

---
+++ src/controllers/github_webhooks.go
@@ -0,0 +1,12 @@
+package controllers
+
+type WebhooksController struct {
+	MainController
+}
+
+/**
+ * TODO 参照https://www.jianshu.com/p/f5965345a802实现Post函数, 需要与getPayloadOfRegisterGithubWebhooks函数一起完成
+ */
+func (this *WebhooksController) Post() {
+
+}
