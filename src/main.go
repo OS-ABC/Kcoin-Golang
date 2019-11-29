84681adc3407d817c92066c105e6e570f073f4c3
Darkone0 <jialc@pku.edu.cn>
2019-11-29 11:13:50 +0800

    添加项目前后端对接

--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"Kcoin-Golang/src/models"
 	_ "Kcoin-Golang/src/routers"
+	"fmt"
 
 	"github.com/astaxie/beego"
+
 )
 
 func main() {
+	fmt.Println(models.GetGithubRepos("Darkone0"))
 	beego.Run()
 }
--- src/models/getGithubRepos.go
+++ src/models/getGithubRepos.go
@@ -2,23 +2,24 @@ package models
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-type GithubRepos []struct {
+type GithubRepos[] struct {
 	Name string `json:"name"`
 	Url string `json:"html_url"`
 }
 
-type ReposInfo struct {
-	ErrorCode string `json:"error_code"`
-	Data GithubRepos `json:"data"`
-}
+//type ReposInfo struct {
+//	ErrorCode string `json:"error_code"`
+//	Data GithubRepos `json:"data"`
+//}
 //获取一个用户在github上所有公开的repository，返回json
-func getGithubRepos(user string)(string, error){
-	var reposInfo ReposInfo
-	reposInfo.ErrorCode = "default Error"
+func GetGithubRepos(user string)(string, error){
+	//var reposInfo ReposInfo
+	//reposInfo.ErrorCode = "default Error"
 	var url string = "https://api.github.com/users/" + user + "/repos"
 
 	client := &http.Client{}
@@ -29,13 +30,21 @@ func getGithubRepos(user string)(string, error){
 		panic(err)
 	}
 	var repos GithubRepos
+	var projects ProjectInfo
 	err1 := json.Unmarshal([]byte(body), &repos)
 	if err1 != nil{
 		panic(err1)
 	}
-	reposInfo.ErrorCode = "0"
-	reposInfo.Data = repos
-	res,err2 := json.Marshal(&reposInfo)
+	for i := range repos {
+		p := &Project{}
+		p.ProjectUrl = repos[i].Url
+		p.ProjectName = repos[i].Name
+		projects.Data = append(projects.Data, p)
+	}
+	//reposInfo.ErrorCode = "0"
+	//reposInfo.Data = repos
+	//res,err2 := json.Marshal(&reposInfo)
+	res,err2 := json.Marshal(&projects)
 	if err2 != nil{
 		panic(err2)
 	}
@@ -43,6 +52,6 @@ func getGithubRepos(user string)(string, error){
 }
 
 //测试输出
-//func main()  {
-//	fmt.Println(getGithubRepos("OS-ABC"))
-//}
+func main()  {
+	fmt.Println(GetGithubRepos("Darkone0"))
+}
