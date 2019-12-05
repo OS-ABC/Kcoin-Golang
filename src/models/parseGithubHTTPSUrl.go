a9fec9dd7e669a25b2893d7acdf18c159f4cb2c1
zhang2j <double_j@pku.edu.cn>
2019-12-05 15:09:29 +0800

    fixes #177 modified ErrorCode

--- src/models/parseGithubHTTPSUrl.go
+++ src/models/parseGithubHTTPSUrl.go
@@ -4,10 +4,10 @@ import (
 	"strings"
 )
 
-func ParseGithubHTTPSUrl(url string) (userName string, userRepo string, ErrorCode int) {
+func ParseGithubHTTPSUrl(url string) (userName string, userRepo string, ErrorCode error) {
 	splitedUrl := strings.Split(url, "/")
 	userName = splitedUrl[3]
 	userRepo = strings.Split(splitedUrl[4], ".")[0] //处理url最后可能出现的.git
-	ErrorCode = 0
+	ErrorCode = nil                                 //还未定义错误类型，先返回nil
 	return userName, userRepo, ErrorCode
 }
