47e07deae7818c1a06c0d967bff6ec18277b1c07
fangfangZF <zflove2011@126.com>
2019-12-05 21:09:26 +0800

    更新授权登录存入信息的方式

--- src/controllers/authorize.go
+++ src/controllers/authorize.go
@@ -21,20 +21,29 @@ func (c *AuthoController) Get() {
 	// 修改参数
 	GithubUser.setGithubUserAccessToken(id,name, accessToken)
 	uri := text.Data.Uri
-
+	time = time.Now().Format("2006-01-02 15:04:05.000000")
+	
 	o := orm.NewOrm()
 	o.Using("default")
 //  移到model 改成GitID查询
 	res, _ := models.FinduserByGitId(id)
 	if res == nil {
-		//insertSql := `INSERT INTO "K_User" (USER_NAME,REGISTER_TIME,HEAD_SHOT_URL,GITHUB_USER_ID) VALUES (?,now(),?,?);`
-		//_, err := o.Raw(insertSql, name, uri, id).Exec()
+		insertSql := `INSERT INTO "K_User" (USER_NAME,REGISTER_TIME,HEAD_SHOT_URL,GITHUB_USER_ID) VALUES (?,?,?,?);`
+		_, err := o.Raw(insertSql, name, uri, time, id).Exec()
 		err := models.InsertUser(name,uri,id)
 
 		if err != nil {
 			panic(err)
 		}
+	}else {
+		updateSql := `update "K_User" set register_time = ? where user_name = ?`
+		_, err := o.Raw(updateSql, time, name).Exec()
+		if err != nil {
+			panic(err)
+		}
 	}
+
+
 	//存储用户名到cooike中，获取语法：c.Ctx.GetCookie("userName")
 	c.Ctx.SetCookie("userName", text.Data.Name, 3600)
 	//存储用户头像url到cooike中，获取语法：c.Ctx.GetCookie("userName")
