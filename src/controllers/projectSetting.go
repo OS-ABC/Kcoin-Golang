94a38850f54cf794071407cc7b78911750e143a2
anaana35 <xl04754442@outlook.com>
2019-12-07 16:24:20 +0800

    升级

--- src/controllers/projectSetting.go
+++ src/controllers/projectSetting.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"Kcoin-Golang/src/models"
 	_ "Kcoin-Golang/src/models"
 
 	"github.com/astaxie/beego"
@@ -11,8 +12,32 @@ type ProjectSettingController struct {
 }
 
 func (c *ProjectSettingController) Get() {
-	id := c.Ctx.Input.Param(":id")
+	//id := c.Ctx.Input.Param(":id")
+	id := c.GetSession(":id")
+	if id == nil {
+		id = c.Ctx.Input.Param(":id")
+		c.SetSession(":id", id)
+	}
 	c.Data["id"] = id
+
+	fakeURL := "https://github.com/Darkone0/weatherForcast"
+
+	// starNum := models.GetStarNum(fakeURL)
+	// contributorsNum := models.GetContributorNum(fakeURL)
+	starNum := c.GetSession("starNum")
+	if starNum == nil {
+		starNum = models.GetStarNum(fakeURL)
+		c.SetSession("starNum", starNum)
+	}
+
+	contributorsNum := c.GetSession("contributorsNum")
+	if contributorsNum == nil {
+		contributorsNum = models.GetContributorNum(fakeURL)
+		c.SetSession("contributorsNum", contributorsNum)
+	}
+
+	c.Data["starNum"] = starNum
+	c.Data["contributorsNum"] = contributorsNum
 	c.TplName = "projectSetting.html" //该controller对应的页面
 
 	//session获取textfiled
