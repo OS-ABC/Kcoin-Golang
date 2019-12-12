1c922c6a94e6405df37c72712dd3fd21c91276fd
TMZup <zhangjianfei@pku.edu.cn>
2019-12-12 11:19:32 +0800

    添加了解析url的一部分错误处理，划水凑工作量？

--- src/models/parseGithubHTTPSUrl.go
+++ src/models/parseGithubHTTPSUrl.go
@@ -1,13 +1,14 @@
 package models
 
 import (
+	"fmt"
 	"strings"
 )
 
-func ParseGithubHTTPSUrl(url string) (userName string, userRepo string, ErrorCode error) {
+func ParseGithubHTTPSUrl(url string) (userName string, userRepo string,  err error) {
 	splitedUrl := strings.Split(url, "/")
+	if len(splitedUrl) != 5 {return "", "", fmt.Errorf("invalid url")}
 	userName = splitedUrl[3]
 	userRepo = strings.Split(splitedUrl[4], ".")[0] //处理url最后可能出现的.git
-	ErrorCode = nil                                 //还未定义错误类型，先返回nil
-	return userName, userRepo, ErrorCode
+	return userName, userRepo, nil
 }
