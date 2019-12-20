ae254cd3a654fa65b840491af893855e283ef220
zhang2j <double_j@pku.edu.cn>
2019-12-20 19:56:28 +0800

    test

--- src/models/ccInject.go
+++ src/models/ccInject.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+
 	"github.com/astaxie/beego/orm"
 )
 
@@ -9,13 +10,13 @@ func CCInject(donatorId int, projId int, cc float64, dtype int) error {
 	o := orm.NewOrm()
 	err := o.Using("default")
 	if err != nil {
-		return fmt.Errorf("database connection error:"+err.Error())
+		return fmt.Errorf("database connection error:" + err.Error())
 	}
 	SQLQuery := `INSERT INTO "k_cc_donate_record" (donate_from, donate_into, donate_cc, donate_type, donate_time)
-				VALUES (?, ?, ?, ?, "CURRENT_TIMESTAMP(0)")`
+				VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP(0))`
 	_, err = o.Raw(SQLQuery, donatorId, projId, cc, dtype).Exec()
 	if err != nil {
-		return fmt.Errorf("database insert error: "+err.Error())
+		return fmt.Errorf("database insert error: " + err.Error())
 	}
 	err = nil
 	return err
